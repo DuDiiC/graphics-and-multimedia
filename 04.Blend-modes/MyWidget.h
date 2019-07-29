90748e892409ff0cf40ccdd2197d8e1fd2d6b197
Maciej Dudek <maciejdudek96@wp.pl>
2019-07-29 13:22:20 +0200

    Add comments and GUI elements in main

--- 04.Blend-modes/MyWidget.h
+++ 04.Blend-modes/MyWidget.h
@@ -6,6 +6,7 @@
 #include <QPainter>
 #include <QColor>
 #include <QString>
+
 #include <vector>
 #include <cmath>
 #include <algorithm>
@@ -18,23 +19,69 @@ class MyWidget : public QWidget {
 
     Q_OBJECT
 
+    /**
+     * IMAGE_PATHS - array with strings with paths to images for blending (default resolution 640 x 480 px)
+     */
     static std::string IMAGE_PATHS[10];
 
     QImage *img;
-    QColor color;
 
     std::vector < Layer > layers;
 
-    // blend modes
+    /** -- BLEND MODES -- */
+    /**
+     * alphaBlending -  function to blend given images
+     * @param image1 -  first (background layer) to blend
+     * @param image2 -  second (foreground layer) to blend
+     * @param alpha -   alpha value of image2
+     * @return new image after alpha blending
+     */
     QImage* alphaBlending(QImage *image1, QImage *image2, int alpha);
+
+    /**
+     * normalMode -     blend mode, in which the second image overwrites the first one
+     */
     QImage* normalMode(QImage *image1, QImage *image2);
+
+    /**
+     * multiplyMode -   blend mode, in which a darker image is usually obtained
+     */
     QImage* multiplyMode(QImage *image1, QImage *image2);
+
+    /**
+     * screenMode -     blend mode, in which a lighter image is usually obtained
+     */
     QImage* screenMode(QImage *image1, QImage *image2);
+
+    /**
+     * overlayMode -    combination of multiply and screen modes, in which light fragments are lighter,
+     *                  but dark ones are darker
+     */
     QImage* overlayMode(QImage *image1, QImage *image2);
+
+    /**
+     * darkenMode -     using a darker of two colors
+     */
     QImage* darkenMode(QImage *image1, QImage *image2);
+
+    /**
+     * lightenMode -    using a ligher of two colors
+     */
     QImage* lightenMode(QImage *image1, QImage *image2);
+
+    /**
+     * differenceMode - using absolute value of difference between colors
+     */
     QImage* differenceMode(QImage *image1, QImage *image2);
+
+    /**
+     * additiveMode -   using white or sum of two colors
+     */
     QImage* additiveMode(QImage *image1, QImage *image2);
+
+    /**
+     * subtractiveMode - using diffrence between sum of colors and white or black
+     */
     QImage* subtractiveMode(QImage *image1, QImage *image2);
 
 public:
@@ -43,6 +90,9 @@ public:
 
     ~MyWidget();
 
+    /**
+     * mixLayers - function mixing all layers with layer's parameters
+     */
     void mixLayers();
 
     Layer getLayer(int index);
--- 04.Blend-modes/main.cpp
+++ 04.Blend-modes/main.cpp
@@ -5,22 +5,62 @@
 #include <QHBoxLayout>
 #include <QVBoxLayout>
 #include <QLabel>
+#include <QRadioButton>
 
 #include "MyWidget.h"
+
+void addBlendModes(QComboBox *comboBox);
 
 int main(int argc, char **argv) {
 
     QApplication app(argc, argv);
 
     QWidget *widget = new QWidget();
-    widget->setMinimumWidth(700);
+    widget->setMinimumWidth(800);
     widget->setMinimumHeight(500);
 
-
+    // widget with image
     MyWidget *myWidget = new MyWidget(640, 480);
+
+    // buttons to choose a layer
+    QRadioButton *layer1 = new QRadioButton("Layer 1");
+    layer1->setMaximumSize(80, 30);
+    layer1->setChecked(true);
+
+    QRadioButton *layer2 = new QRadioButton("Layer 2");
+    layer2->setMaximumSize(80, 30);
+
+    QRadioButton *layer3 = new QRadioButton("Layer 3");
+    layer3->setMaximumSize(80, 30);
+
+    // container for buttons
+    QHBoxLayout *radioButtons = new QHBoxLayout();
+    radioButtons->addWidget(layer1);
+    radioButtons->addWidget(layer2);
+    radioButtons->addWidget(layer3);
+
+    // comboBox to choose blend mode
+    QComboBox *blendMode =new QComboBox();
+    addBlendModes(blendMode);
+
+    // slider for alpha value
+    QHBoxLayout *alpha = new QHBoxLayout();
+    QLabel *alphaText = new QLabel("alpha value");
+    QSlider *alphaSlider = new QSlider(Qt::Horizontal);
+    alphaSlider->setRange(0, 100);
+    alphaSlider->setValue(100);
+    alpha->addWidget(alphaText);
+    alpha->addWidget(alphaSlider);
+
+    // container for user interface elements
+    QVBoxLayout *gui = new QVBoxLayout();
+    gui->addLayout(radioButtons);
+    gui->addWidget(blendMode);
+    gui->addLayout(alpha);
 
     QHBoxLayout *layout = new QHBoxLayout();
     layout->addWidget(myWidget, 800);
+    layout->addLayout(gui);
 
     /* show app layout */
     widget->setLayout(layout);
@@ -31,4 +71,16 @@ int main(int argc, char **argv) {
     // ...
 
     return app.exec();
+}
+
+void addBlendModes(QComboBox *comboBox) {
+    comboBox->addItem("normal");
+    comboBox->addItem("multiply");
+    comboBox->addItem("screen");
+    comboBox->addItem("overlay");
+    comboBox->addItem("darken");
+    comboBox->addItem("lighten");
+    comboBox->addItem("difference");
+    comboBox->addItem("additive");
+    comboBox->addItem("subtractive");
 }
