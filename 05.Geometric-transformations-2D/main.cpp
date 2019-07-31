b304a31e85cfd1b6eb2d399641d400f7a4da682f
Maciej Dudek <maciejdudek96@wp.pl>
2019-07-31 15:01:02 +0200

    Add GUI for 2D transformation

---
+++ 05.Geometric-transformations-2D/main.cpp
@@ -0,0 +1,131 @@
+#include <qapplication.h>
+#include <QWidget>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QButtonGroup>
+#include <QRadioButton>
+#include <QSlider>
+#include <QCheckBox>
+#include <QLabel>
+
+int main(int argc, char **argv ) {
+
+    QApplication app( argc, argv );
+
+    // main app widget
+    QWidget *widget = new QWidget();
+    widget->setMinimumWidth(800);
+    widget->setMinimumHeight(800);
+
+    // ---INTERPOLATION---
+    // buttons to choose a interpolation mode
+    QLabel *interpolationLabel = new QLabel("INTERPOLATION MODE");
+
+    QRadioButton *nearestNeighborButton = new QRadioButton("nearest neighbor");
+    nearestNeighborButton->setMaximumSize(200, 30);
+    nearestNeighborButton->setChecked(true);
+
+    QRadioButton *doubleLineInterpolationButton = new QRadioButton("double-line interpolation");
+    doubleLineInterpolationButton->setMaximumSize(200, 30);
+
+    QButtonGroup *interpolationButtonGroup = new QButtonGroup();
+    interpolationButtonGroup->addButton(nearestNeighborButton);
+    interpolationButtonGroup->setId(nearestNeighborButton, 0);
+    interpolationButtonGroup->addButton(doubleLineInterpolationButton);
+    interpolationButtonGroup->setId(doubleLineInterpolationButton, 1);
+
+    QVBoxLayout *interpolationLayout = new QVBoxLayout();
+    interpolationLayout->addWidget(interpolationLabel);
+    interpolationLayout->addWidget(nearestNeighborButton);
+    interpolationLayout->addWidget(doubleLineInterpolationButton);
+
+    // ---ROTATION---
+    QLabel *rotationLabel = new QLabel("ROTATION");
+
+    QSlider *rotationSlider = new QSlider(Qt::Horizontal);
+    rotationSlider->setRange(-360, 360); // TODO: set range
+
+    // ---TRANSLATION---
+    QLabel *translationLabel = new QLabel("TRANSLATION");
+
+    QLabel *translationXLabel = new QLabel("x");
+    QSlider *translationXSlider = new QSlider(Qt::Horizontal);
+    //translationXSlider->setRange(); // TODO: set range
+
+    QHBoxLayout *translationXLayout = new QHBoxLayout();
+    translationXLayout->addWidget(translationXLabel);
+    translationXLayout->addWidget(translationXSlider);
+
+    QLabel *translationYLabel = new QLabel("y");
+    QSlider *translationYSlider = new QSlider(Qt::Horizontal);
+    //translationYSlider->setRange(); // TODO: set range
+
+    QHBoxLayout *translationYLayout = new QHBoxLayout();
+    translationYLayout->addWidget(translationYLabel);
+    translationYLayout->addWidget(translationYSlider);
+
+    // ---SCALING---
+    QLabel *scalingLabel = new QLabel("TRANSLATION");
+    QCheckBox *homogeneousScalingCheckBox = new QCheckBox("homogeneous scaling");
+
+    QLabel *scalingXLabel = new QLabel("x");
+    QSlider *scalingXSlider = new QSlider(Qt::Horizontal);
+    //scalingXSlider->setRange(); // TODO: set range
+
+    QHBoxLayout *scalingXLayout = new QHBoxLayout();
+    scalingXLayout->addWidget(scalingXLabel);
+    scalingXLayout->addWidget(scalingXSlider);
+
+    QLabel *scalingYLabel = new QLabel("y");
+    QSlider *scalingYSlider = new QSlider(Qt::Horizontal);
+    //scalingYSlider->setRange(); // TODO: set range
+
+    QHBoxLayout *scalingYLayout = new QHBoxLayout();
+    scalingYLayout->addWidget(scalingYLabel);
+    scalingYLayout->addWidget(scalingYSlider);
+
+    // ---SHEARING---
+    QLabel *shearingLabel = new QLabel("SHEARING");
+
+    QLabel *shearingXLabel = new QLabel("x");
+    QSlider *shearingXSlider = new QSlider(Qt::Horizontal);
+    //shearingXSlider->setRange(); // TODO: set range
+
+    QHBoxLayout *shearingXLayout = new QHBoxLayout();
+    shearingXLayout->addWidget(shearingXLabel);
+    shearingXLayout->addWidget(shearingXSlider);
+
+    QLabel *shearingYLabel = new QLabel("y");
+    QSlider *shearingYSlider = new QSlider(Qt::Horizontal);
+    //shearingYSlider->setRange(); // TODO: set range
+
+    QHBoxLayout *shearingYLayout = new QHBoxLayout();
+    shearingYLayout->addWidget(shearingYLabel);
+    shearingYLayout->addWidget(shearingYSlider);
+
+    QVBoxLayout *guiLayout = new QVBoxLayout();
+
+    guiLayout->addLayout(interpolationLayout);
+
+    guiLayout->addWidget(rotationLabel);
+    guiLayout->addWidget(rotationSlider);
+
+    guiLayout->addWidget(translationLabel);
+    guiLayout->addLayout(translationXLayout);
+    guiLayout->addLayout(translationYLayout);
+
+    guiLayout->addWidget(scalingLabel);
+    guiLayout->addWidget(homogeneousScalingCheckBox);
+    guiLayout->addLayout(scalingXLayout);
+    guiLayout->addLayout(scalingYLayout);
+
+    guiLayout->addWidget(shearingLabel);
+    guiLayout->addLayout(shearingXLayout);
+    guiLayout->addLayout(shearingYLayout);
+
+    widget->setLayout(guiLayout);
+
+    widget->show();
+
+    return app.exec();
+}
